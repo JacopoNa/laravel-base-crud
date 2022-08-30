c4509ca1e38266f1c706b82b74c292f7a7382bf3
JacopoNa <j.nardelli@virgilio.it>
2022-08-30 16:14:37 +0200

    layout and HomeController

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Comics</h1>
+        
+        <ul>
+            <li>HomePage</li>
+            <li>Catalogo dei fumetti</li>
+        </ul>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
