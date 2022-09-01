adf35b91f5e46e87987e8a0190faac08ea228427
JacopoNa <j.nardelli@virgilio.it>
2022-09-01 17:32:38 +0200

    fix validation

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -20,43 +20,43 @@
 
         <div>
             <label for="title">Titolo</label>
-            <input type="text" id="title" name="title" value="{{ old('title') }}">
+            <input type="text" id="title" name="title" value="{{ old('title') ?  old('title') : $comic->title }}">
         </div>
         <br>
 
         <div>
             <label for="thumb">Url immagine</label>
-            <input type="text" id="thumb" name="thumb" value="{{ old('thumb') }}">
+            <input type="text" id="thumb" name="thumb" value="{{ old('thumb') ?  old('thumb') : $comic->thumb }}">
         </div>
         <br>
 
         <div>
             <label for="description">Descrizione</label>
-            <input type="text" id="description" name="description" value="{{ old('description') }}">
+            <input type="text" id="description" name="description" value="{{ old('description') ?  old('description') : $comic->description }}">
         </div>
         <br>
 
         <div>
             <label for="series">Serie</label>
-            <input type="text" id="series" name="series" value="{{ old('series') }}">
+            <input type="text" id="series" name="series" value="{{ old('series') ?  old('series') : $comic->series }}">
         </div>
         <br>
 
         <div>
             <label for="type">Tipo</label>
-            <input type="text" id="type" name="type" value="{{ old('type') }}">
+            <input type="text" id="type" name="type" value="{{ old('type') ?  old('type') : $comic->type }}">
         </div>
         <br>
 
         <div>
             <label for="sale_date">Sale date</label>
-            <input type="text" id="sale_date" name="sale_date" value="{{ old('sale_date') }}">
+            <input type="text" id="sale_date" name="sale_date" value="{{ old('sale_date') ?  old('sale_date') : $comic->sale_date }}">
         </div>
         <br>
 
         <div>
             <label for="price">Prezzo</label>
-            <input type="text" id="price" name="price" value="{{ old('price') }}">
+            <input type="text" id="price" name="price" value="{{ old('price') ?  old('price') : $comic->price }}">
         </div>
         <br>
 
